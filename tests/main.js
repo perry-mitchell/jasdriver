41f024f22ef866bd3a5fc7e00fe5e554d3053ed2
Perry Mitchell <perry@perrymitchell.net>
2016-04-12 21:39:04 +0300

    Testing base

---
+++ tests/main.js
@@ -0,0 +1,11 @@
+describe("JasDriver", function() {
+
+    it("runs tests and passes", function() {
+        expect(1).toBe(1);
+    });
+
+    xit("can fail too", function() {
+        expect(1).toBe(2);
+    });
+
+});
